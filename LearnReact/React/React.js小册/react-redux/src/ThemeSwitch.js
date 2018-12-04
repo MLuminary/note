ccda2a02516d3964b151e1a4af6825c52d03574a
Hutchins <418037025@qq.com>
2018-12-04 11:18:21 +0800

    feat: contxt 与 store 结合

--- LearnReact/React/React.js小册/react-redux/src/ThemeSwitch.js
+++ LearnReact/React/React.js小册/react-redux/src/ThemeSwitch.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ThemeSwitch extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  // dispatch action 去改变颜色
+  handleSwitchColor = (color) => {
+    const { store } = this.context
+    store.dispatch({
+      type: 'CHANGE_COLOR',
+      themeColor: color
+    })
+  }
+
   render () {
     return (
       <div>
-        <button>Red</button>
-        <button>Blue</button>
+        <button onClick={() => this.handleSwitchColor('red')}>Red</button>
+        <button onClick={() => this.handleSwitchColor('blue')}>Blue</button>
       </div>
     )
   }
--- LearnReact/React/React.js小册/react-redux/src/index.js
+++ LearnReact/React/React.js小册/react-redux/src/index.js
@@ -5,7 +5,48 @@ import Header from './Header'
 import Content from './Content'
 import './index.css'
 
+function reducer(state, action) {
+  if (!state) {
+    return {
+      themeColor: 'red'
+    }
+  }
+
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+function createStore(reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  dispatch({})
+  return { getState, dispatch, subscribe }
+}
+
+let store = createStore(reducer)
+
+
 class Index extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  getChildContext() {
+    // Context
+    return { store }
+  }
+
   render () {
     return (
       <div>
