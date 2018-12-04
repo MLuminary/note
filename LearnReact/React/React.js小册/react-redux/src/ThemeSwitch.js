81a28d0b00596a199ed59dc68e1a14394af579ee
Hutchins <418037025@qq.com>
2018-12-04 23:26:09 +0800

    feat: mapDispatchToProps

--- LearnReact/React/React.js小册/react-redux/src/ThemeSwitch.js
+++ LearnReact/React/React.js小册/react-redux/src/ThemeSwitch.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
 
 class ThemeSwitch extends Component {
-  static contextTypes = {
-    store: PropTypes.object
+  static propTypes = {
+    themeColor: PropTypes.string,
+    onSwitchColor: PropTypes.func
   }
 
-  // dispatch action 去改变颜色
-  handleSwitchColor = (color) => {
-    const { store } = this.context
-    store.dispatch({
-      type: 'CHANGE_COLOR',
-      themeColor: color
-    })
+  handleSwitchColor (color) {
+    if (this.props.onSwitchColor) {
+      this.props.onSwitchColor(color)
+    }
   }
 
   render () {
     return (
       <div>
-        <button onClick={() => this.handleSwitchColor('red')}>Red</button>
-        <button onClick={() => this.handleSwitchColor('blue')}>Blue</button>
+        <button
+          onClick={() => this.handleSwitchColor('red')}>Red</button>
+        <button
+          onClick={() => this.handleSwitchColor('blue')}>Blue</button>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
 
 export default ThemeSwitch
--- LearnReact/React/React.js小册/react-redux/src/react-redux.js
+++ LearnReact/React/React.js小册/react-redux/src/react-redux.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 
-export const connect = (mapStateToProps) => (WrappedComponent) => {
+export const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
   class Connect extends React.Component{
     static contextTypes = {
       store: PropTypes.object
@@ -21,7 +21,14 @@ export const connect = (mapStateToProps) => (WrappedComponent) => {
 
     _updateProps () {
       const { store } = this.context
-      let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
+      let stateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), this.props)
+        : {} // 额外传入 props，让获取数据更加灵活方便
+
+      let dispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(store.dispatch, this.props)
+        : {} // 防止 mapDispatchToProps 没有传入
+
       this.setState({
         allProps: { // 整合普通的 props 和从 state 生成的 props
           ...stateProps,
