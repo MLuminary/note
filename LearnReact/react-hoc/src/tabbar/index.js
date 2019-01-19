f7bb2624a913ef714955d6dc1e518866f04f65cb
Hutchins <418037025@qq.com>
2019-01-19 20:41:35 +0800

    end

---
+++ LearnReact/react-hoc/src/tabbar/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TabBar extends Component {
+  render() {
+    return (
+      <div className='tab-bar'>
+        tab-bar
+      </div>
+    )
+  }
+}
+
+export default TabBar
