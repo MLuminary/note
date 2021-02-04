2c7463503fc4b3ebd9940fff455e58dae0ded376
MLuminary <418037025@qq.com>
2021-02-04 23:05:57 +0800

    chore: fomart category

--- LearnReact/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const http = require('http');
-
-var app = express();
-
-http.createServer(app).listen(8080);
-
-app.use(express.static('./React'));
--- LearnReact/readme.md
+++
@@ -1,5 +0,0 @@
-# LearnReact
-
-具体的实例和笔记都在 React 文件中 :monkey:
-
-在终端中运行 `node app.js` 可以在地址上输入对应连接来查看文件 `React` 中的 demo，比如看 React 中 demo 就输入 `localhost:8080/demo/`
